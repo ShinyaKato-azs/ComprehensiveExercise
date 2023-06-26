b7b23e81aa797065168a007d4c8c5ec8e7cc8d75
Shinya-Kato <s-kato@a-zs.co.jp>
2023-06-26 17:04:24 +0900

    DbTest.Javaを削除、applicationpropertiesにJPAの依存関係を追加、リポジトリの作成、itemsをエンティティクラスとして修正

--- ComprehensiveExercise/src/main/java/com/example/DbTest/DbTest.java
+++
@@ -1,9 +0,0 @@
-package com.example.DbTest;
-
-public class DbTest {
-
-	public static void main(String[] args) {
-
-	}
-
-}
--- ComprehensiveExercise/src/main/java/com/example/Vegetables/Items.java
+++ ComprehensiveExercise/src/main/java/com/example/Vegetables/Items.java
@@ -1,10 +1,16 @@
 package com.example.Vegetables;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "items")
 public class Items {
 
+	@Id
 	private Integer id;
 	private String name;
 
