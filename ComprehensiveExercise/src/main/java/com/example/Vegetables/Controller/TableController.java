9bc947faf396a8419d178dd3e07e1c377e48418b
Shinya-Kato <s-kato@a-zs.co.jp>
2023-06-26 17:28:25 +0900

    TableControllerの修正。/vegetablesにGETリクエストが来たら、DBのitemsテーブルから取得したレコードの情報がログで流れる

--- ComprehensiveExercise/src/main/java/com/example/Vegetables/Controller/TableController.java
+++ ComprehensiveExercise/src/main/java/com/example/Vegetables/Controller/TableController.java
@@ -8,19 +8,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.Service.ApplicationService;
+import com.example.Service.ItemServiceImpl;
 import com.example.Vegetables.Items;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class TableController {
 
 	@Autowired
 	private ApplicationService applicationService;
+	@Autowired
+	private ItemServiceImpl itemServiceImpl;
 
 	@GetMapping("/vegetables")
 	public String getVegetables(Model model) {
 
 		List<Items> Items = applicationService.MakeVegetableArray();
 		model.addAttribute("Items", Items);
+
+		List<Items> DbItems = itemServiceImpl.getItems();
+		log.info(DbItems.toString());
 
 		return "Vegetables/vegetables";
 
