37cbd6e72cfc046fb5c15ac454d283eb47035a2a
Shinya-Kato <s-kato@a-zs.co.jp>
2023-06-27 09:45:34 +0900

    postされたらリダイレクトをかけるようにコントローラーを修正

--- ComprehensiveExercise/src/main/java/com/example/Vegetables/Controller/TableController.java
+++ ComprehensiveExercise/src/main/java/com/example/Vegetables/Controller/TableController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.Service.ItemServiceImpl;
 //import com.example.Service.MakeArray;
@@ -36,6 +37,12 @@ public class TableController {
 
 		return "Vegetables/vegetables";
 
+	}
+
+	@PostMapping("/vegetables")
+	public String postVegetables() {
+
+		return "redirect:/vegetables";
 	}
 
 }
