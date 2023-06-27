b293796cb84ff6d33974bf5f13105131634bc6be
Shinya-Kato <s-kato@a-zs.co.jp>
2023-06-27 15:30:49 +0900

    formのフィールドに@NotBlankアノテーションを追加

--- ComprehensiveExercise/src/main/java/com/example/Vegetables/Form/AddForm.java
+++ ComprehensiveExercise/src/main/java/com/example/Vegetables/Form/AddForm.java
@@ -1,11 +1,13 @@
 package com.example.Vegetables.Form;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 //フォームから受け取った内容をバインドするためのクラス
 @Data
 public class AddForm {
 
+	@NotBlank
 	private String name;
 
 }
