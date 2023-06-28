c8d6d7992da9ad2f572089e86b03500c8eb43026
Shinya-Kato <s-kato@a-zs.co.jp>
2023-06-28 13:31:05 +0900

    バリデーションのアノテーションを間違えていたため修正（@Max→@Size）

--- ComprehensiveExercise/src/main/java/com/example/Vegetables/Form/AddForm.java
+++ ComprehensiveExercise/src/main/java/com/example/Vegetables/Form/AddForm.java
@@ -1,7 +1,7 @@
 package com.example.Vegetables.Form;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 //フォームから受け取った内容をバインドするためのクラス
@@ -9,7 +9,7 @@ import lombok.Data;
 public class AddForm {
 
 	@NotBlank
-	@Max(4)
+	@Size(min = 0, max = 4)
 	private String name;
 
 }
