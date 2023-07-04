0c3125af1c7c708f2aceb32f389fe4579fc31562
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-04 17:13:14 +0900

    DetailFormクラスを作成、Detail.htmlのformとバインディング

--- ComprehensiveExercise/src/main/java/com/example/Vegetables/Controller/DetailController.java
+++ ComprehensiveExercise/src/main/java/com/example/Vegetables/Controller/DetailController.java
@@ -3,9 +3,12 @@ package com.example.Vegetables.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.Service.ItemService;
+import com.example.Vegetables.Form.DetailForm;
 
 @Controller
 public class DetailController {
@@ -15,13 +18,21 @@ public class DetailController {
 
 	//詳細画面を返す
 	@GetMapping("/detail/{Id}")
-	public String getVegetablesDetail(@PathVariable("Id") Integer Id) {
+	public String getVegetablesDetail(@PathVariable("Id") Integer Id, @ModelAttribute DetailForm detailForm) {
 
 		//主キーで探して商品を1件返す
 		itemService.getItemOne(Id);
 
 		return "Vegetables/vegetablesDetail";
 
+	}
+
+	@PostMapping("/detail/update")
+	public String updateItem() {
+
+		String string = "hoge";
+
+		return string;
 	}
 
 }
---
+++ ComprehensiveExercise/src/main/java/com/example/Vegetables/Form/DetailForm.java
@@ -0,0 +1,11 @@
+package com.example.Vegetables.Form;
+
+import lombok.Data;
+
+//フォームから受け取った内容をバインドするためのクラス
+@Data
+public class DetailForm {
+
+	private String name;
+
+}
