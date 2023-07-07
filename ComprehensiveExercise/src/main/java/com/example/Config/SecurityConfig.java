b9d9f8beb74c2f981d6e7e6e6530c02c46d8c9f4
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-07 17:37:06 +0900

    springbootセキュリティを導入、新規登録ページのみ直リンクを有効

---
+++ ComprehensiveExercise/src/main/java/com/example/Config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.example.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests()
+				.requestMatchers("/signup").permitAll()
+				.anyRequest().authenticated();
+
+		return http.build();
+	}
+
+}
