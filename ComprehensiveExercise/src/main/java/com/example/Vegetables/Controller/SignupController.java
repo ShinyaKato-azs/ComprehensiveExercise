ac6203fa88688a7d6f5e216bb68db94f5537baa3
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-07 14:24:22 +0900

    フォームクラスのインスタンスをエンティティクラスにコピー

--- ComprehensiveExercise/src/main/java/com/example/Vegetables/Controller/SignupController.java
+++ ComprehensiveExercise/src/main/java/com/example/Vegetables/Controller/SignupController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.User.User;
 import com.example.Vegetables.Form.SignupForm;
 
 import lombok.extern.slf4j.Slf4j;
@@ -14,20 +15,21 @@ import lombok.extern.slf4j.Slf4j;
 public class SignupController {
 
 	@GetMapping("/signup")
-
 	public String getSignup(@ModelAttribute SignupForm form) {
 
 		return ("user/signup");
 	}
 
 	@PostMapping("/signup")
-
 	public String postSignup(@ModelAttribute SignupForm form) {
 
 		//登録処理
+		User signupUser = new User();
+		signupUser.setUserId(form.getUserId());
+		signupUser.setPassword(form.getPassword());
 
 		//ログ出力
-		log.info(form.toString());
+		log.info(signupUser.toString());
 
 		return "redirect:/signup";
 	}
