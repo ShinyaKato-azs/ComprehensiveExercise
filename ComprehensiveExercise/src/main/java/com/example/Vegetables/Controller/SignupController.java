fee8c717c04f68c7c79d51827f049352754d3a3e
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-07 14:20:49 +0900

    htmlをthymeleaf化、フォームクラスとバインディングされてコントローラーに渡せていることを確認

--- ComprehensiveExercise/src/main/java/com/example/Vegetables/Controller/SignupController.java
+++ ComprehensiveExercise/src/main/java/com/example/Vegetables/Controller/SignupController.java
@@ -2,15 +2,34 @@ package com.example.Vegetables.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.Vegetables.Form.SignupForm;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@Slf4j
 public class SignupController {
 
 	@GetMapping("/signup")
 
-	public String getSignup() {
+	public String getSignup(@ModelAttribute SignupForm form) {
 
-		return ("/user/signup");
+		return ("user/signup");
+	}
+
+	@PostMapping("/signup")
+
+	public String postSignup(@ModelAttribute SignupForm form) {
+
+		//登録処理
+
+		//ログ出力
+		log.info(form.toString());
+
+		return "redirect:/signup";
 	}
 
 }
