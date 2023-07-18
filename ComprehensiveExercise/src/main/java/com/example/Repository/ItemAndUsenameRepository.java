f7ec29b6ed02755e34472a35b144c2c7c14f16f9
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-18 10:47:13 +0900

    商品IDで指定して、商品＋農家名を１件取得するクエリとメソッドを追加

--- ComprehensiveExercise/src/main/java/com/example/Repository/ItemAndUsenameRepository.java
+++ ComprehensiveExercise/src/main/java/com/example/Repository/ItemAndUsenameRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.Vegetables.ItemAndUsername;
 
@@ -20,5 +21,19 @@ public interface ItemAndUsenameRepository extends JpaRepository<ItemAndUsername,
 			+ "    INNER JOIN items as i "
 			+ "        ON u.user_id = i.farmer_id; ", nativeQuery = true)
 	public List<ItemAndUsername> findItems();
+
+	@Query(value = "select "
+			+ "    user_name as farmer_name "
+			+ "    , id as item_id "
+			+ "    , i.name "
+			+ "    , i.price "
+			+ "    , i.farmer_id "
+			+ "from "
+			+ "    v_user u "
+			+ "    INNER JOIN items as i "
+			+ "        ON u.user_id = i.farmer_id "
+			+ "where "
+			+ "    i.id = :itemId ; ", nativeQuery = true)
+	public ItemAndUsername getOneItem(@Param("itemId") Integer itemId);
 
 }
