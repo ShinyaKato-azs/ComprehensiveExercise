cc8c635d5692fc4b986f4fc8e01af3be22ce6462
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-21 13:51:34 +0900

    農家詳細画面のエンドポイントと、htmlを仮作成（/detail/farmer/数字　でアクセスされたら、hello worldのページを返す）

---
+++ ComprehensiveExercise/src/main/java/com/example/User/Controller/FarmerDetailController.java
@@ -0,0 +1,17 @@
+package com.example.User.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class FarmerDetailController {
+
+	@GetMapping("/detail/farmers/{farmerId}")
+	public String getFarmersDetail(@PathVariable("farmerId") Integer farmerId) {
+
+		return "/user/farmerDetail";
+
+	}
+
+}
