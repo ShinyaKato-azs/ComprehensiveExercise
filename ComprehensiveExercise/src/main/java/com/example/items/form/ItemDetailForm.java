9c1e7655c95b470b0d24df49f907304903222c86
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-27 17:47:08 +0900

    更新時に、名前の文字数の制限を撤廃

--- ComprehensiveExercise/src/main/java/com/example/items/form/ItemDetailForm.java
+++ ComprehensiveExercise/src/main/java/com/example/items/form/ItemDetailForm.java
@@ -1,7 +1,6 @@
 package com.example.items.form;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 //フォームから受け取った内容をバインドするためのクラス
@@ -10,7 +9,6 @@ public class ItemDetailForm {
 
 	private Integer id;
 	@NotBlank
-	@Size(min = 0, max = 4)
 	private String name;
 	private Integer price;
 	private Integer farmerId;
