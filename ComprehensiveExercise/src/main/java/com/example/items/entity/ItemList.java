cfe041d6bf99c182a5b2b7b6ee3f344f931a46ba
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-28 11:40:34 +0900

    商品エンティティ、商品一覧・詳細画面エンティティのフィールドにjavadocコメントを追加

--- ComprehensiveExercise/src/main/java/com/example/items/entity/ItemList.java
+++ ComprehensiveExercise/src/main/java/com/example/items/entity/ItemList.java
@@ -7,18 +7,30 @@ import lombok.Data;
 
 @Entity
 @Data
-/** 商品一覧画面のエンティティクラス */
+/** 
+ * 商品一覧画面のエンティティクラス
+ * 商品テーブルとユーザーテーブルを農家IDで結合して、農家名を取得するクエリの結果を受け取っている
+ */
 public class ItemList {
 
+	/** 商品ID  */
 	@Id
 	@Column(name = "item_id")
 	private Integer itemId;
+
+	/** 商品名  */
 	@Column(name = "name")
 	private String name;
+
+	/** 価格  */
 	@Column(name = "price")
 	private Integer price;
+
+	/** 農家ID：商品を登録した農家のユーザーID  */
 	@Column(name = "farmer_id")
 	private Integer farmerId;
+
+	/** 農家名  */
 	@Column(name = "farmer_name")
 	private String farmerName;
 
--- ComprehensiveExercise/src/main/java/com/example/items/entity/Items.java
+++ ComprehensiveExercise/src/main/java/com/example/items/entity/Items.java
@@ -13,10 +13,17 @@ import lombok.Data;
 /** 商品のエンティティクラス */
 public class Items {
 
+	/** 商品ID（DB側の処理で自動的に連番で振られる） */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	/** 商品名 */
 	private String name;
+
+	/** 価格 */
 	private Integer price;
+
+	/** 農家ID：商品を登録した農家のユーザーID */
 	private Integer farmerId;
 }
