dcba319befd65382ef4aea564a5ab80e210cb398
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-28 13:23:43 +0900

    農家詳細・ユーザーエンティティクラスのフィールドにjavadocコメントを追加

--- ComprehensiveExercise/src/main/java/com/example/user/entity/FarmerDetail.java
+++ ComprehensiveExercise/src/main/java/com/example/user/entity/FarmerDetail.java
@@ -13,11 +13,17 @@ import lombok.Data;
 /** 農家詳細画面のエンティティクラス */
 public class FarmerDetail {
 
+	/** 農家ID */
 	@Column(name = "farmer_id")
 	private Integer farmerId;
+
+	/** 農家名 */
 	@Column(name = "farmer_name")
 	private String farmerName;
-	//取り扱っている野菜名前
+
+	/** 
+	 * 取り扱い商品名
+	 *  */
 	@Id
 	@Column(name = "farmer_items_name")
 	private String farmerItemsName;
--- ComprehensiveExercise/src/main/java/com/example/user/entity/VUser.java
+++ ComprehensiveExercise/src/main/java/com/example/user/entity/VUser.java
@@ -15,12 +15,28 @@ import lombok.Data;
 @Table(name = "v_user")
 /** ユーザーのエンティティクラス */
 public class VUser {
+
+	/** 
+	 * ユーザーID
+	 * DB側で自動的に連番を振られる
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer userId;
+
+	/** 
+	 * 登録メールアドレス 
+	 * SpringSecurityではusername属性として設定している
+	 */
 	private String userMail;
+
+	/** ユーザー名 */
 	private String userName;
+
+	/** パスワード */
 	private String password;
+
+	/** 権限（農家もしくは一般ユーザー） */
 	private String authority;
 
 }
