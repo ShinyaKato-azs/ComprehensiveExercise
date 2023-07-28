866bde1662a7a13bdba8a507cd66ab6b440224cb
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-28 13:28:00 +0900

    農家詳細・ユーザー新規登録のフォームのフィールドにjavadocコメントを追加

--- ComprehensiveExercise/src/main/java/com/example/user/form/FarmerDetailForm.java
+++ ComprehensiveExercise/src/main/java/com/example/user/form/FarmerDetailForm.java
@@ -6,7 +6,10 @@ import lombok.Data;
 /** 農家詳細画面にデータを渡すためのクラス */
 public class FarmerDetailForm {
 
+	/** 農家ID */
 	private Integer farmerId;
+
+	/** 農家名 */
 	private String farmerName;
 
 }
--- ComprehensiveExercise/src/main/java/com/example/user/form/SignupForm.java
+++ ComprehensiveExercise/src/main/java/com/example/user/form/SignupForm.java
@@ -11,16 +11,40 @@ import lombok.Data;
 /** ログイン画面のフォームとバインドするためのクラス */
 public class SignupForm {
 
+	/** ユーザーID */
 	private Integer userId;
+
+	/** 
+	 * メールアドレス
+	 * 空文字や半角スペースのみは不可
+	 * メールアドレス形式のみを許可
+	 */
 	@NotBlank
 	@Email
 	private String userMail;
+
+	/** 
+	 * パスワード
+	 * 空文字や半角スペースのみは不可
+	 * 大文字小文字の英数字＋記号のみ許可
+	 * 4文字以上100文字以下
+	 */
 	@NotBlank
 	@Length(min = 4, max = 100)
 	@Pattern(regexp = "^[a-zA-Z0-9]+$")
 	private String password;
+
+	/** 
+	 * ユーザー名
+	 * 空文字や半角スペースのみは不可
+	 */
 	@NotBlank
 	private String userName;
+
+	/** 
+	 * 権限
+	 * ラジオボタンで必ず選択する
+	 */
 	@NotBlank
 	private String authority;
 
