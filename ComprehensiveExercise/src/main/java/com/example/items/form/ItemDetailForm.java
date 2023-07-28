acf801011df19bc02bb7fb38dadbf0d1d02060dc
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-28 11:58:53 +0900

    商品詳細画面のフォームクラスのフィールドにjavadocコメントを追加、価格フィールドのバリデーションを追加

--- ComprehensiveExercise/src/main/java/com/example/items/form/ItemDetailForm.java
+++ ComprehensiveExercise/src/main/java/com/example/items/form/ItemDetailForm.java
@@ -1,5 +1,6 @@
 package com.example.items.form;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
@@ -7,11 +8,27 @@ import lombok.Data;
 @Data
 public class ItemDetailForm {
 
+	/** 商品ID */
 	private Integer id;
+	/**
+	 *商品名
+	 *空文字や半角スペースのみは不可
+	 *更新処理では、文字数の制限なし
+	*/
 	@NotBlank
 	private String name;
+
+	/**
+	 *価格
+	 *マイナスの値は不可
+	*/
+	@Min(0)
 	private Integer price;
+
+	/** 農家ID */
 	private Integer farmerId;
+
+	/** 農家名 */
 	private String farmerName;
 
 }
