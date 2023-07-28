92d11d547be323985298764b23115dd1efe27571
Shinya-Kato <s-kato@a-zs.co.jp>
2023-07-28 11:54:01 +0900

    商品追加フォームのフィールドにjavadocコメントを追加。価格フィールドにバリデーションが必要なことに気が付いたため、0以上の値に限定するバリデーションを追加。併せて、htmlの価格フォームのtype属性をtext→numberに変更。

--- ComprehensiveExercise/src/main/java/com/example/items/form/ItemAddForm.java
+++ ComprehensiveExercise/src/main/java/com/example/items/form/ItemAddForm.java
@@ -1,5 +1,6 @@
 package com.example.items.form;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -8,9 +9,20 @@ import lombok.Data;
 @Data
 public class ItemAddForm {
 
+	/**
+	 *商品名
+	 *空文字や半角スペースのみは不可
+	 *文字数は4文字まで
+	*/
 	@NotBlank
 	@Size(min = 0, max = 4)
 	private String name;
+
+	/**
+	 * 価格
+	 * マイナスの値は不可
+	 * */
+	@Min(0)
 	private Integer price;
 	private Integer farmerId;
 
